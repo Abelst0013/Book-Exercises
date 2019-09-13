40e50eb8ccb94152b1db17017fc8e7a04b6bdd20
Abel st <abel_st_99@outlook.com>
2019-09-13 00:26:29 -0500

    Capitulo 2 commit

---
+++ AbelLibro/Capitulo 2/main.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <array>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <stdio.h>
+#include <conio.h>
+#include <cstdlib>
+
+
+using namespace std;
+
+// Exercises 1 
+
+//int main() // 
+//{
+//	cout << "Hello, programming!\n"
+//		<< "Here we go!";
+//	return 0;
+//}
+
+// Exercises 2 
+
+//int main() //
+//{
+//	cout << "How to get to the bathroom.\n"
+//		<< "\t1. Walk down the hall.\n"
+//		<< "\t2. On the right will be the stairs.\n"
+//		<< "\t3. Climb the stairs.\n"
+//		<< "\t4. When you finish climbing you will find the bathroom on the right\n";
+//	return 0;
+//}
+
+// Exercises 3 
+
+//int main() //
+//{
+//	cout << "How to get to the school from Abel's house.\n"
+//		<< "\t1. Dirigete al sur por Dr. Carlos Villaseñor hacia Calle Rayon(30 metros).\n"
+//		<< "\t2. Gira a la derecha con dirección a Calle Rayon(50 metros).\n"
+//		<< "\t3. Gira a la izquierda con la dirección a Calle Maria de Jesús Romo de Vivar(450 metros).\n"
+//		<< "\t4. Continúa por 5(600 metros).\n"
+//		<< "\t5. Continúa por Bandera Nacional (300 metros).\n"
+//		<< "\t6. Gira a la izquierda con dirección a Avenida Manuel Ávila Camacho(1.7 kilometros).\n"
+//		<< "\t7. Gira a la derecha con dirección a Av. Cvln. Jorge Álvarez del Castillo.(700 metros).\n"
+//		<< "\t8. Gira a la izquierda con dirección a Calle Mar de Bering(200 metros).\n"
+//		<< "\t9. Gira a la derecha con dirección a Calle Andrés Terán(190 metros).\n"
+//		<< "\t10. Gira a la izquierda hacia Calle Andrés Terán(90 metros).\n"
+//		<< "\t11. Gira levemente a la izquierda con dirección a Calle Andrés Terán(350 metros).\n"
+//		<< "\t12. Entra en la rotonda(20 metros).\n"
+//		<< "\tUniversidad de Artes Digitales.\n" 
+//		<< "\tCalle Andrés Terán 1106, Chapultepec.\n"
+//		<< "\tCountry, 44620 Guadalajara, Jal.\n";
+//	return 0;
+//}
+
+// Exercises 4 
+
+int main()
+{
+	cout << "Como preparar sincronizadas paso a paso\n"
+		<< "\n"
+		<< "Ingredientes:\n"
+		<< "\t1. Tortillas.\n"
+		<< "\t2. Jamon.\n"
+		<< "\t3. Jitomate(Opcional).\n"
+		<< "\t4. Queso de mesa o para fundir.\n"
+		<< "\n";
+
+	cout << "Preparacion:\n"
+		<< "\t1. Coger un sartén del tamaño de las tortillas y poner a calentar sin necesidad de utilizar aceite.\n"
+		<< "\t   Poner al fuego la tortilla y esperar un par de minutos para que tenga color, sin que se queme.\n"
+		<< "\t2. Picar el Jamón y el queso en pequeños tamaños. Esparcir el jamón y el queso picado sobre la mitad de la tortilla.\n"
+		<< "\t3. Doblar la tortilla por la mitad y dar la vuelta para que se queme un poco, y se derrita el queso al mismo tiempo.\n"
+		<< "\t4. Sacar la sincronizada del sartén y servirlas recién hechas, con un poco de jitomate picado por dentro.\n"
+		<< "\n"
+		<< "Buen Apetito!\n";
+}

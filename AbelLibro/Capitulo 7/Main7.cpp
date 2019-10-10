6e276dac6fc64e68ddc4e8c442db59efbec7fe47
Abel st <abel_st_99@outlook.com>
2019-10-10 04:35:20 -0500

    Estructura del ejercicio 1 y creo varios mas tambien.....
    Mas tarde comienzo :anguished:

--- AbelLibro/Capitulo 7/Main7.cpp
+++ AbelLibro/Capitulo 7/Main7.cpp
@@ -10,3 +10,53 @@
 #include <cstdlib>
 
 using namespace std;
+
+// Exercises 1
+
+class Token
+{
+public:
+	char type;
+	double value;
+	string names;
+};
+
+class Token_stream
+{
+public:
+	Token get();
+	void putback(Token t);
+private:
+	bool full{ false };
+	Token buf;
+};
+
+void Token_stream::putback(Token t)
+{
+
+}
+
+Token Token_stream::get()
+{
+
+}
+
+Token_stream ts;
+double expression();
+
+double primary() {
+
+}
+
+double term() {
+
+}
+
+double expression() {
+
+}
+
+int main()
+{
+
+}

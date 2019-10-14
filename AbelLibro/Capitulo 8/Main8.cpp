646ecc12829e1a0d4d7315d96ecaed6a4d01e4aa
Abel st <abel_st_99@outlook.com>
2019-10-14 02:58:44 -0500

    Cap8
    creacion solamente

---
+++ AbelLibro/Capitulo 8/Main8.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <array>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <stdio.h>
+#include <conio.h>
+#include <cstdlib>
+
+using namespace std;
